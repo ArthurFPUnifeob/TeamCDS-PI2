75d53d15e82b36aec6c75d6ae862d81df5ddb225
arthurfp16 <afpeluque@gmail.com>
2023-09-20 19:33:17 -0300

    chorus: add lib e codigo para auto reload

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const { app, BrowserWindow } = require('electron')
-const path = require('path')
+
 
 function createWindow () {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      nodeIntegration: true
     }
   })
 
@@ -23,8 +23,12 @@ app.whenReady().then(() => {
   })
 })
 
-app.on('window-all-closed', () => {
+  app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
+
+try {
+  require('electron-reloader')(module)
+} catch (_) {}
