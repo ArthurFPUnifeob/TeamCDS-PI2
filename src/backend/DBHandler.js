28ae8167b4a9a54c5af0a411812322916161eebc
arthurfp16 <afpeluque@gmail.com>
2023-10-26 17:28:56 -0300

    26102023

--- src/backend/DBHandler.js
+++ src/backend/DBHandler.js
@@ -1,3 +1,4 @@
+import { isArray } from "chart.js/dist/helpers/helpers.core.js";
 import { DBConnect } from "./DBConnect.js"
 import envvars from  "./env.js"
 
@@ -5,8 +6,23 @@ class DBHandler extends DBConnect {
     constructor(host, user, password, database) {
         super(host, user, password, database);
     }
-    insert(table, attributeName, values){
-        this.requisition(`insert into ${table} (${attributeName}, ${values})`)
+    zip(array1, array2){
+        return array1.map((k, i) => [k, array2[i]])
+    }
+    insert(arrTableName, arrColumsName, arrValues){
+        /* arrTableName, arrColumsName, arrValues - devem ser do tipo array */
+        if(isArray(arrColumsName) && isArray(arrTableName) && isArray(arrValues)){
+            this.requisition(`INSERT INTO ${arrTableName} (${arrColumsName}) VALUES ${arrValues};`)
+        }else{
+            throw new Error("DBHandles.insert - todos os parametros devem ser arrays"); 
+        }
+    }
+    update(arrTableName, arrColumsName, arrValues, condition){
+
+
+
+        
+        this.requisition(`update ${arrTableName} (${arrColumsName}) VALUES ${arrValues};`)
     }
 }
 const DBHandler = new DBHandler(envvars[0], envvars[1], envvars[2], envvars[3])
